7f849388a8c24f223e159b3b512caab3044382a0
mur4d1n <mur4d1n@yandex.ru>
2025-05-14 02:03:58 +0300

    Канонические урлы для постов

--- blog/views.py
+++ blog/views.py
@@ -10,7 +10,14 @@ def post_list(request: HttpRequest) -> HttpResponse:
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 
-def post_detail(request: HttpRequest, id: int) -> HttpResponse:
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+def post_detail(request: HttpRequest, year: int, month: int, day: int, post: str) -> HttpResponse:
+    post = get_object_or_404(
+        Post,
+        status=Post.Status.PUBLISHED,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day,
+    )
 
     return render(request, 'blog/post/detail.html', {'post': post})
