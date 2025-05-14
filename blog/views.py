7cea32d105130ab1e492406c55c6c5feda4b7840
mur4d1n <mur4d1n@yandex.ru>
2025-05-15 01:52:29 +0300

    Переходим на CBV

--- blog/views.py
+++ blog/views.py
@@ -1,34 +1,32 @@
-from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
-from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404
+from django.views.generic import ListView, DetailView
 
 from .models import Post
 
 
-def post_list(request: HttpRequest) -> HttpResponse:
-    post_list = Post.published.all()
-
-    paginator = Paginator(post_list, 5)
-    page_number = request.GET.get('page', 1)
-
-    try:
-        posts = paginator.page(page_number)
-    except PageNotAnInteger:
-        posts = paginator.page(1)
-    except EmptyPage:
-        posts = paginator.page(paginator.num_pages)
-
-    return render(request, 'blog/post/list.html', {'posts': posts})
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 
-def post_detail(request: HttpRequest, year: int, month: int, day: int, post: str) -> HttpResponse:
-    post = get_object_or_404(
-        Post,
-        status=Post.Status.PUBLISHED,
-        slug=post,
-        publish__year=year,
-        publish__month=month,
-        publish__day=day,
-    )
+class PostDetailView(DetailView):
+    model = Post
+    context_object_name = 'post'
+    template_name = 'blog/post/detail.html'
 
-    return render(request, 'blog/post/detail.html', {'post': post})
+    def get_object(self, queryset=None):
+        year = self.kwargs['year']
+        month = self.kwargs['month']
+        day = self.kwargs['day']
+        slug = self.kwargs['post']
+
+        return get_object_or_404(
+            Post,
+            status=Post.Status.PUBLISHED,
+            slug=slug,
+            publish__year=year,
+            publish__month=month,
+            publish__day=day,
+        )
