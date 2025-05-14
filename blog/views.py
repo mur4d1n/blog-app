6b00eb6c915d259d871c5fdef39ab9eb2054bce0
mur4d1n <mur4d1n@yandex.ru>
2025-05-14 13:28:18 +0300

    Пагинация для блога

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, get_object_or_404
 
@@ -5,7 +6,17 @@ from .models import Post
 
 
 def post_list(request: HttpRequest) -> HttpResponse:
-    posts = Post.published.all()
+    post_list = Post.published.all()
+
+    paginator = Paginator(post_list, 5)
+    page_number = request.GET.get('page', 1)
+
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
 
     return render(request, 'blog/post/list.html', {'posts': posts})
 
